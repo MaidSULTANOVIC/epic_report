1b544a181970deb879941c8a116e8f07f469971a
Robin Brumbter <rbrumbter@unistra.fr>
2021-12-15 22:03:28 +0100

    Completed T1View

--- lib/view/t1View.dart
+++ lib/view/t1View.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:epic_report/controllers/report.controller.dart';
+import 'package:flutter/services.dart';
 
 class T1View extends StatefulWidget {
   @override
@@ -17,73 +18,220 @@ class _T1ViewState extends State<T1View> {
     super.initState();
   }
 
-  int _value = 0;
-  bool checkedValue = false;
+  String typeChangedValue = "Fire";
+  String setChangedValue = "-";
+  bool checkedValueImmunity = false;
+  bool checkedValueOutspeed = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Container(
-            child: Column(
-      children: [
-        Row(
-          children: [
-            Text("Speed of your first character :"),
-            SizedBox(width: 3),
-            Flexible(child: TextField()),
-          ],
-        ),
-        Column(
-          children: [
-            Text("First character"),
-            Row(
-              children: [
-                Text("Name :"),
-                SizedBox(width: 3),
-                Flexible(child: TextField()),
-                SizedBox(width: 3),
-                Text("Type :"),
-                SizedBox(width: 3),
-                Text("Dropdown"),
-                SizedBox(width: 3),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Artefact :"),
-                SizedBox(width: 3),
-                Flexible(child: TextField()),
-                SizedBox(width: 3),
-                Text("HP :"),
-                SizedBox(width: 3),
-                Flexible(child: TextField()),
-                SizedBox(width: 3),
-              ],
-            ),
-            Row(
-              children: [
-                Flexible(
+            child: SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Speed of your first character :"),
+              SizedBox(width: 5),
+              Flexible(
+                  child: Container(
+                width: 50.0,
+                child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '200',
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(3),
+                    ]),
+              )),
+            ],
+          ),
+          Character("First"),
+          Character("Second"),
+          Character("Third")
+        ],
+      ),
+    )));
+  }
+
+  Container Character(String enemyNum) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(3.0),
+      child: Column(
+        children: [
+          Text(
+            enemyNum + " enemy",
+            style: TextStyle(fontSize: 20),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 200,
+                child: Flexible(
+                    child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                        ),
+                        keyboardType: TextInputType.name,
+                        textCapitalization: TextCapitalization.sentences,
+                        inputFormatters: [
+                      LengthLimitingTextInputFormatter(20),
+                    ])),
+              ),
+              SizedBox(width: 10),
+              Text("Type :"),
+              SizedBox(width: 5),
+              DropdownButton<String>(
+                value: typeChangedValue,
+                items: <String>['Fire', 'Ice', 'Earth', 'Light', 'Dark']
+                    .map((String valueType) {
+                  return DropdownMenuItem<String>(
+                    value: valueType,
+                    child: Center(
+                        child: Text(
+                      valueType,
+                      textAlign: TextAlign.center,
+                    )),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    typeChangedValue = value!;
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 200,
+                child: Flexible(
+                    child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Artifact',
+                        ),
+                        keyboardType: TextInputType.name,
+                        textCapitalization: TextCapitalization.sentences,
+                        inputFormatters: [
+                      LengthLimitingTextInputFormatter(20),
+                    ])),
+              ),
+              SizedBox(width: 10),
+              Flexible(
+                  child: Container(
+                //width: 80.0,
+                child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'HP',
+                      hintText: '10000',
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(5),
+                    ]),
+              )),
+              SizedBox(width: 3),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 160.0,
+                child: Flexible(
                     child: CheckboxListTile(
-                        title: Text("Outspeed?"),
-                        value: checkedValue,
+                        title: Text(
+                          "Enemy outspeed",
+                          textAlign: TextAlign.center,
+                        ),
+                        value: checkedValueOutspeed,
                         onChanged: (value) {
                           setState(() {
-                            checkedValue = value!;
+                            checkedValueOutspeed = value!;
                           });
                         })),
-                SizedBox(width: 3),
-                Text("HP :"),
-                SizedBox(width: 3),
-                Flexible(child: TextField()),
-                SizedBox(width: 3),
-              ],
-            ),
-            Row(
-              children: [],
-            ),
-          ],
-        )
-      ],
-    )));
+              ),
+              Flexible(
+                  child: Container(
+                width: 50.0,
+                child: TextField(
+                    decoration:
+                        InputDecoration(labelText: 'CR%', hintText: '0-99'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(2),
+                    ]),
+              )),
+              SizedBox(width: 10),
+              Text(
+                "(Speed is calculated\nautomatically)",
+                style: TextStyle(fontSize: 10),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 160.0,
+                child: Flexible(
+                    child: CheckboxListTile(
+                        title: Text(
+                          "Immunity",
+                          textAlign: TextAlign.center,
+                        ),
+                        value: checkedValueImmunity,
+                        onChanged: (value) {
+                          setState(() {
+                            checkedValueImmunity = value!;
+                          });
+                        })),
+              ),
+              Text("Other set :"),
+              SizedBox(width: 5),
+              DropdownButton<String>(
+                value: setChangedValue,
+                items: <String>['-', 'Counter', 'Lifesteal', 'Injury']
+                    .map((String valueSet) {
+                  return DropdownMenuItem<String>(
+                    value: valueSet,
+                    child: Center(
+                        child: Text(
+                      valueSet,
+                      textAlign: TextAlign.center,
+                    )),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    setChangedValue = value!;
+                  });
+                },
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                  child: TextField(
+                      minLines: 1,
+                      maxLines: 2,
+                      decoration: InputDecoration(
+                        labelText: 'Comment',
+                      ),
+                      keyboardType: TextInputType.text,
+                      textCapitalization: TextCapitalization.sentences,
+                      inputFormatters: [
+                    LengthLimitingTextInputFormatter(60),
+                  ])),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
